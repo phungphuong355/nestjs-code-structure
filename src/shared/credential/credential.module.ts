c40c60b9a8c2864681a8376bdadf44e583365bb2
phungphuong355 <phungphuong355@gmail.com>
2023-02-28 23:39:34 +0700

    add shared module

---
+++ src/shared/credential/credential.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { CredentialService } from "./credential.service";
+
+@Module({
+  providers: [CredentialService],
+  exports: [CredentialService],
+})
+export class CredentialModule {}

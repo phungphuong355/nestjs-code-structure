3709693818018fa47660a6fa2abbbf53051c8e4a
phungphuong355 <phungphuong355@gmail.com>
2023-03-09 11:05:58 +0700

    update

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -14,6 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(username: string, password: string): Promise<Payload> {
     const user = await this._authService.validateUser(username, password);
 
-    return { userId: user.id, username: user.username, roles: user.roles };
+    return { userId: user.userId, username: user.username, roles: user.roles };
   }
 }

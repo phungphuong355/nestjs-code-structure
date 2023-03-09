ed89e3c361fd70ea1860e0edc2be4aa24846811e
phungphuong355 <phungphuong355@gmail.com>
2023-03-10 01:45:22 +0700

    update

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,10 +1,25 @@
 import { INestApplication } from "@nestjs/common";
+// import compression from "compression";
+// import session from "express-session";
+// import passport from "passport";
 
 export function middleware(app: INestApplication): INestApplication {
+  // app.use(compression());
+  // app.use(
+  //   session({
+  //     secret: "structure",
+  //     resave: false,
+  //     saveUninitialized: true,
+  //     cookie: { secure: false },
+  //   }),
+  // );
+  // app.use(passport.initialize());
+  // app.use(passport.session());
+
   // CORS
-  app.enableCors({
-    methods: ["GET", "PUT", "PATCH", "POST", "DELETE", "UPDATE", "OPTIONS"],
-  });
+  // app.enableCors({
+  //   methods: ["GET", "PUT", "PATCH", "POST", "DELETE", "UPDATE", "OPTIONS"],
+  // });
 
   return app;
 }
